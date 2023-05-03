2f8aacf997c9ca0e0e084a28d5fba06487005e66
fllamas <chewfasa5@hotmail.com>
2023-05-03 16:21:48 -0700

    Added project introduction and credits. Also added the project zip file used at the time of this lab, to check integrity.

--- 1-5.sh
+++ 1-5.sh
@@ -25,11 +25,19 @@ case $choice in
         echo "Exiting.."
         ;;
     1)
-        echo "Introducing the Project...."
+        echo -e "Introducing the Project....
+          Survival Slayer is a 2.5D top-down survival slasher game coded in 
+          Python using Pygame, with assets created using Aseprite and Tiled.
+          The game features challenging gameplay where players must fight off
+          skull-like entities that pursue and attack them. The player has a
+          collection of weapons and magic spells at their disposal, each with
+          their unique attack sounds. Throughout the game, the map will dynamically
+          change over time, indicating the passage of time and introducing new
+          waves of enemies to keep the player engaged and challenged. Additionally,
+          there is background music playing to enhance the overall game experience."
         ;;
     2) 
         read -p "Enter value of either cksum, sha1sum, or md5sum: " choice2
-        #read 1-3.sh
         ./1-3.sh "$choice2"
         ;;
         
@@ -38,6 +46,14 @@ case $choice in
         ./1-4.sh
         ;;
     4)
-        echo "Rolling the credits..."
+        echo -e "Rolling the credits...\n
+          Survival Slayer (CPSC 254 Project)
+          Alfredo Llamas, Long Calamia, and Elexzandreia Livermore
+          Some sprites and maps are original.\n
+          Some sprites, 1 map, and music:
+          Pixel-Boy and AAA
+          Link to their Website: https://pixel-boy.itch.io/ninja-adventure-asset-pack
+          Their Patreon: https://www.patreon.com/pixelarchipel
+          Their License used: released under the Creative Commons Zero (CC0) license."
         ;;
 esac
