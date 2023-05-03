8955fab7d8f528d2a70710265683e6093e4cf2ae
Elexzandreia <elexzandreia@csu.fullerton.edu>
2023-05-02 22:24:35 -0700

    1-5.sh completed

--- 1-5.sh
+++ 1-5.sh
@@ -28,42 +28,16 @@ case $choice in
         echo "Introducing the Project...."
         ;;
     2) 
-        read -p "Enter your number selection: " choice2
-        CKSUM="2030573888 6684332"
-        SHA1SUM="a77093d7910936419a7630f7ce20dd8100ae71f4"
-        MD5SUM="11b323eae5527cbbeb2bd327b689b45d"
-
-        if [ "$CKSUM" = choice2 ]; then
-            echo "Success, integrity verified: cksum match."
-        elif [ "$SHA1SUM" = choice2 ]; then
-            echo "Success, integrity verified: sha1sum match."
-        elif [ "$MD5SUM" = choice2 ]; then
-            echo "Success, integrity verified: md5sum match."
-        else 
-            echo "Error, corrupt or wrong zip file. Please check correct downloaded zip."
-        
+        read -p "Enter value of either cksum, sha1sum, or md5sum: " choice2
+        #read 1-3.sh
+        ./1-3.sh "$choice2"
         ;;
-
-    3) 
-        echo "checking Dependencies..."
-        if command -V python3 >/dev/null 2>&1 ; then
-            echo "python3 found"
-            echo "version: $(python3 -V)"
-        else
-            echo "python3 not found"
         
-        if command -V pip >/dev/null 2>&1 ; then
-            echo "pip found"
-            echo "version: $(pip -V)"
-        else
-            echo "pip not found. Use: apt install python3-pip"
-        if command pip show pygame &>/dev/null; then
-            echo "pygame found"
-            echo "version: $(pip show pygame)"
-        else
-            echo "pygame not found. Use: python3 -m pip install -U pygame --user"
+    3)
+        echo "checking Dependencies..."
+        ./1-4.sh
         ;;
     4)
         echo "Rolling the credits..."
-        
+        ;;
 esac
